7c8cc1dec8d6823ff2bf73b4da81a2a2a175c56e
Fred Cheng <fredrocious@gmail.com>
2017-03-30 11:16:46 -0700

    fix for data serialization, enumerateBytes parameters have changed in swift 3.1: the second block parameter is an index into the bytes array, not the length

--- Crypto/Data+Crypto.swift
+++ Crypto/Data+Crypto.swift
@@ -63,8 +63,8 @@ extension Data {
 
 	var hex: String {
 		var string = ""
-		enumerateBytes { pointer, count, _ in
-			for i in 0..<count {
+		enumerateBytes { pointer, offset, _ in
+			for i in offset..<pointer.count {
 				string += String(format: "%02x", pointer[i])
 			}
 		}
